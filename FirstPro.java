2e1712634e0cdb848da5ff089ef049933b59ad34
unknown <jenipathmanathan3@gmail.com>
2020-09-19 14:33:24 +0530

    multiplication of 3 numbers

---
+++ FirstPro.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class FirstPro
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		System.out.println("Enter 3 numbers");
+		int ans = sc.nextInt();
+		for(int i = 1; i<=3; i++)
+		{
+			sum = sum + i;
+		}
+		System.out.println("sum is " + sum);
+		
+		int prod = 1;
+		System.out.println("Enter 3 numbers");
+		int ans = sc.nextInt();
+		for(int i = 1; i<=3; i++)
+		{
+			prod = prod * i;
+		}
+		System.out.println("Product is " + prod);
+	}
+}
