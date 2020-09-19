866e62694327fc36e6e85b01b7fe960015e1fe41
unknown <jenipathmanathan3@gmail.com>
2020-09-19 12:04:57 +0530

    hungry prompt

--- hungry.java
+++ hungry.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
 class FirstPro
 {
 	public static void main(String args[])
 	{
-		System.out.println("Eat samosa");
-		System.out.println("Eat pizza");
-		
-		
-		
-		System.out.println("sleep well");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Are you hungry?");
+		String ans = sc.nextLine();
+		if(ans.equals("Y") || ans.equals("y"))
+		{
+			System.out.println("Eat samosa");
+		}
+		if(ans.equals("N") || ans.equals("n"))
+		{
+			System.out.println("Do your Homework");
+		}
 	}
 }
